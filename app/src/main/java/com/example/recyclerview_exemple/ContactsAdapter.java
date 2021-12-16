a8c2504dd69aaf1d3f9b01764586e88928fab873
truyen.lv <truyen.lv@THTC188>
2021-12-16 11:47:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview_exemple/ContactsAdapter.java
@@ -0,0 +1,95 @@
+package com.example.recyclerview_exemple;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Create the basic adapter extending from RecyclerView.Adapter
+// Note that we specify the custom ViewHolder which gives us access to our views
+public class ContactsAdapter extends
+        RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+    // ... view holder defined above...
+
+    // Store a member variable for the contacts
+    private List<Contact> mContacts;
+
+    // Pass in the contact array into the constructor
+    public ContactsAdapter(List<Contact> contacts) {
+        mContacts = contacts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data model based on position
+        Contact contact = mContacts.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = holder.nameTextView;
+        textView.setText(contact.getName());
+        Button button = holder.messageButton;
+        button.setText(contact.isOnline() ? "Message" : "Offline");
+        button.setEnabled(contact.isOnline());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+        public Button messageButton;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+    }
+
+    public void removeItem(int position) {
+        mContacts.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(Contact item, int position) {
+        mContacts.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public List<Contact> getData() {
+        return mContacts;
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview_exemple/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.recyclerview_exemple;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ArrayList<Contact> contacts;
+    ContactsAdapter adapter;
+    String TAG="rv";
+    ConstraintLayout coordinatorLayout;
+    RecyclerView rvContacts;
+    Button btn_addContact;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Lookup the recyclerview in activity layout
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+        btn_addContact=(Button) findViewById(R.id.btn_addContact);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        adapter = new ContactsAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
+
+        btn_addContact.setOnClickListener(this);
+
+        enableSwipeToDeleteAndUndo();
+    }
+
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+
+                final int position = viewHolder.getAdapterPosition();
+                final Contact item = adapter.getData().get(position);
+
+                adapter.removeItem(position);
+
+
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Item was removed from the list.", Snackbar.LENGTH_LONG);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        adapter.restoreItem(item, position);
+                        rvContacts.scrollToPosition(position);
+                    }
+                });
+
+                snackbar.setActionTextColor(Color.YELLOW);
+                snackbar.show();
+
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(rvContacts);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        // Lấy id từng button
+        switch(v.getId()){
+            case R.id.btn_addContact:
+                // Hiện thị thông báo trong vòng vài giây
+                //Toast.makeText(getApplication(), "Hiệp Sĩ IT: Bạn đang Click vào Button Login", Toast.LENGTH_LONG).show();
+//// Add a new contact
+//                contacts.add(0, new Contact("Barney", true));
+//// Notify the adapter that an item was inserted at position 0
+//                adapter.notifyItemInserted(0);
+
+                //remove
+                contacts.remove(0);
+                adapter.notifyItemMoved(0,0);
+                adapter.notifyDataSetChanged();
+                break;
+
+        }
+    }
+}
